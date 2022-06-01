d4f4c8f13cabadbe536355413ac7695eb9412f1b
iitalics <iitalics@gmail.com>
2022-05-31 21:22:28 -0400

    smalls your icon

--- scripts/resize-icon.bash
+++ scripts/resize-icon.bash
@@ -8,3 +8,7 @@ set -ex
 convert resources/icon/favicon-640x640.png \
         -resize 16x16 \
         resources/icon/favicon-16x16.ico
+
+convert resources/icon/favicon-640x640.png \
+        -resize 160x160 \
+        resources/icon/favicon-160x160.png
