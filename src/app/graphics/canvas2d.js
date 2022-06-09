e647308eaf6dc10f3230ee69b197eaf153514694
iitalics <iitalics@gmail.com>
2022-06-08 21:21:33 -0400

    add new graphics module: rewritten, shows smaller preview pieces

---
+++ src/app/graphics/canvas2d.js
@@ -0,0 +1,125 @@
+import skin from '../../data/skin'
+
+export class Renderer {
+    constructor(canvas, geometry) {
+        this.graphics = canvas.getContext('2d');
+        this.geometry = geometry;
+        this.paths = {
+            gridBack: {
+                color: skin.grid.back,
+                path: geometry.grid.back.path,
+            },
+            gridLines: {
+                color: skin.grid.outline, // FIXME
+                path: geometry.grid.lines.path,
+            },
+            gridBorder: {
+                color: skin.hud.outline, // FIXME
+                path: geometry.grid.border.path,
+            },
+            cursorUp: {
+                transform: { tx: 0, ty: 0 },
+                color: skin.cursor.up,
+                path: geometry.cursor.up.path,
+            },
+            cursorDown: {
+                transform: { tx: 0, ty: 0 },
+                color: skin.cursor.down,
+                path: geometry.cursor.down.path,
+            },
+            cell: {
+                transform: { tx: 0, ty: 0 },
+                color: { fill: null, alpha: 0.0 },
+                path: geometry.cell.path,
+            },
+            mini: {
+                transform: { tx: 0, ty: 0 },
+                color: { fill: null, alpha: 1.0 },
+                path: geometry.mini.path,
+            },
+        };
+    }
+
+    draw(playfield, piece, ghost, queue, cursor) {
+        let { graphics, geometry, paths } = this;
+
+        function draw({ transform, color, path }) {
+            if (transform) {
+                graphics.save();
+                graphics.translate(transform.tx, transform.ty);
+            }
+            graphics.beginPath();
+            let lift = true;
+            for (let pos of path) {
+                if (pos === null) {
+                    lift = true;
+                    continue;
+                }
+                if (lift) {
+                    graphics.moveTo(pos[0], pos[1]);
+                    lift = false;
+                } else {
+                    graphics.lineTo(pos[0], pos[1]);
+                }
+            }
+            graphics.fillStyle = color.fill;
+            graphics.globalAlpha = color.alpha;
+            graphics.fill();
+            if (transform) {
+                graphics.restore();
+            }
+        }
+
+        graphics.globalAlpha = 1.0;
+        graphics.clearRect(0, 0, geometry.width, geometry.height);
+
+        draw(paths.gridBack);
+        draw(paths.gridLines);
+        draw(paths.gridBorder);
+
+        paths.cell.color.alpha = 1.0;
+        for (let cell of playfield.getNonEmptyCells()) {
+            paths.cell.color.fill = skin.block[cell.type].fill;
+            geometry.getCellTransform(cell, paths.cell.transform);
+            draw(paths.cell);
+        }
+
+        if (ghost) {
+            paths.cell.color.alpha = 0.3;
+            paths.cell.color.fill = skin.block[ghost.type].fill;
+            for (let cell of ghost.getCells()) {
+                geometry.getCellTransform(cell, paths.cell.transform);
+                draw(paths.cell);
+            }
+        }
+
+        if (piece) {
+            paths.cell.color.alpha = 1.0;
+            paths.cell.color.fill = skin.block[piece.type].fill;
+            for (let cell of piece.getCells()) {
+                geometry.getCellTransform(cell, paths.cell.transform);
+                draw(paths.cell);
+            }
+        }
+
+        if (queue.hold) {
+            paths.mini.color.fill = skin.block[queue.hold].fill;
+            for (let t of geometry.getHoldCellTransform(queue.hold, paths.mini.transform)) {
+                draw(paths.mini);
+            }
+        }
+        for (let i = 0; i < queue.previews.length; i++) {
+            let type = queue.previews[i];
+            paths.mini.color.fill = skin.block[type].fill;
+            for (let t of geometry.getPreviewCellTransforms(type, i, paths.mini.transform)) {
+                draw(paths.mini);
+            }
+        }
+
+        if (cursor) {
+            let path = cursor.down ? paths.cursorDown : paths.cursorUp;
+            geometry.getCellTransform(cursor.pos, path.transform);
+            draw(path);
+        }
+    }
+}
---
+++ src/app/graphics/index.js
@@ -0,0 +1,156 @@
+import { Matrix } from '../model/matrix'
+import { Queue } from '../model/queue'
+import { Geometry } from './geometry'
+import * as Canvas2D from './canvas2d'
+
+export class BoardGraphics {
+    constructor() {
+        this.onDragStart = undefined;
+        this.onDragEnd = undefined;
+        this.onDrag = undefined;
+
+        this._playfield = Matrix.EMPTY;
+        this._queue = Queue.EMPTY;
+        this._piece = null;
+        this._ghost = null;
+        this._cursor = { pos: null, down: false };
+
+        this._geometry = new Geometry();
+        this._canvas = null;
+        this._renderer = null;
+        this._repaintRequested = false;
+
+        this._onMouseEvent = this._onMouseEvent.bind(this);
+        this._onRepaint = this._onRepaint.bind(this);
+    }
+
+    setQueue(queue) {
+        this._queue = queue;
+        this.repaint();
+    }
+
+    setPlayfield(playfield) {
+        this._playfield = playfield;
+        this._ghost = null;
+        this.repaint();
+    }
+
+    setPiece(piece) {
+        this._piece = piece;
+        this._ghost = null;
+        this.repaint();
+    }
+
+    setCanvas(canvas) {
+        if (this._canvas === canvas) {
+            return;
+        }
+
+        if (this._canvas) {
+            this._canvas.removeEventListener('mousemove', this._onMouseEvent);
+            this._canvas.removeEventListener('mousedown', this._onMouseEvent);
+            this._canvas.removeEventListener('mouseout', this._onMouseEvent);
+            window.removeEventListener('mouseup', this._onMouseEvent);
+        }
+
+        if (!canvas) {
+            this._canvas = null;
+            this._renderer = null;
+            return;
+        }
+
+        this._canvas = canvas;
+        this._canvas.width = this._geometry.width;
+        this._canvas.height = this._geometry.height;
+        this._renderer = new Canvas2D.Renderer(this._canvas, this._geometry);
+        this.repaint();
+
+        this._canvas.addEventListener('mousemove', this._onMouseEvent);
+        this._canvas.addEventListener('mousedown', this._onMouseEvent);
+        this._canvas.addEventListener('mouseout', this._onMouseEvent);
+        window.addEventListener('mouseup', this._onMouseEvent);
+    }
+
+    _onMouseEvent(evt) {
+        if (!this._canvas) {
+            return;
+        }
+
+        let { pos, down } = this._cursor;
+        switch (evt.type) {
+        case 'mousemove':
+        case 'mouseout':
+            {
+                let boundingRect = this._canvas.getBoundingClientRect();
+                let relativeX = evt.clientX - boundingRect.x;
+                let relativeY = evt.clientY - boundingRect.y;
+                pos = this._geometry.getCursorPos(relativeX, relativeY);
+                break;
+            }
+
+        case 'mousedown':
+            down = (pos !== null);
+            break;
+
+        case 'mouseup':
+            down = false;
+            break;
+        }
+
+        if (this._cursor.down !== down) {
+            if (down) {
+                this.onDragStart && this.onDragStart(pos);
+            } else {
+                this.onDragEnd && this.onDragEnd();
+            }
+            this._cursor.down = down;
+            this.repaint();
+        }
+                
+        if (!posEquals(this._cursor.pos, pos)) {
+            if (pos && down) {
+                this.onDrag && this.onDrag(pos);             
+            }
+            this._cursor.pos = pos;
+            this.repaint();
+        }
+    }
+
+    repaint() {
+        if (!this._repaintRequested) {
+            this._repaintRequested = true;
+            requestAnimationFrame(this._onRepaint);
+        }
+    }
+
+    _onRepaint() {
+        this._repaintRequested = false;
+        if (this._renderer && this._canvas) {
+            this._renderer.draw(
+                this._playfield,
+                this._piece,
+                this._ghost ||= this._piece?.sonicDrop(this._playfield),
+                this._queue,
+                this._cursor.pos && this._cursor,
+            );
+
+            this._canvas.style.cursor = getCursorStyle(this._cursor.pos, this._cursor.down);
+        }
+    }
+}
+
+function posEquals(p1, p2) {
+    if (p1 === null) { return p2 === null; }
+    if (p2 === null) { return p1 === null; }
+    return p1.x === p2.x && p1.y === p2.y;
+}
+
+function getCursorStyle(over, down) {
+    if (!over) {
+        return 'unset';
+    }
+    if (!down) {
+        return 'pointer';
+    }
+    return 'grabbing';
+}
